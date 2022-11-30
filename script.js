ba8c09ff1dfbc603b1ecb02118bdb7eee660a5bc
Ana Belen Chamba Mera <abm.chamba@gmail.com>
2022-11-30 11:37:48 -0500

    DOM JavaScript

---
+++ script.js
@@ -0,0 +1,14 @@
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2')
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+
+btn.addEventListener('click', sumarInputValues);
+
+function sumarInputValues(event){
+    const sumaInputs = parseInt(input1.value) + parseInt(input2.value);
+    pResult.innerHTML = 'Resultado ' + ' ' + sumaInputs;
+}
